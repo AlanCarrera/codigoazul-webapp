398cb62b7e407c7ee36d7dc10228d5dc57216b83
rainbow021078 <rainbow021078@gmail.com>
2014-05-19 19:26:46 +0000

    Este es el bueno :)

---
+++ CodeBlueTeamReadyBean3/src/java/messageDrivenBean/MessageBean.java
@@ -0,0 +1,53 @@
+package messageDrivenBean;
+
+
+import javax.annotation.Resource;
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.inject.Inject;
+import javax.jms.JMSConnectionFactory;
+import javax.jms.JMSContext;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.jms.TextMessage;
+import methodology.MethodologyApplier;
+
+@MessageDriven(mappedName = "jms/zoneQueue", activationConfig = {
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jms/zoneQueue")
+})
+public class MessageBean implements MessageListener {
+    @Resource(mappedName = "jms/teamReadyQueue")
+    private Queue teamReadyQueue;
+    @Inject
+    @JMSConnectionFactory("jms/myQueueFactory")
+    private JMSContext context;
+    
+
+
+    public MessageBean() {
+    }
+
+    @Override
+    public void onMessage(Message message) {
+        try {
+            TextMessage tm = (TextMessage) message;
+
+            MethodologyApplier methodology = new MethodologyApplier(tm.getText());
+            methodology.formResponseTeam();
+            
+            //send notice
+            sendJMSMessageToTeamReadyQueue(tm.getText());
+
+            System.out.println("Consumed message: " + tm.getText());
+        } catch (JMSException jex) {
+            System.out.println("Exception: " + jex);
+        }
+    }
+
+    private void sendJMSMessageToTeamReadyQueue(String messageData) {
+        context.createProducer().send(teamReadyQueue, messageData);
+    }
+}
